bf93f2e2fc02fe0beb3ce89140fafc75b8ed4dc6
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2018-06-02 16:57:09 +0100

    scraped first data from stats table

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,9 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
+//Solo betis, de momento
 const links_teams = ['http://www.laligafantasymarca.com/equipos/betis'];
+const base_url = 'http://www.laligafantasymarca.com';
 
 const options = {
   uri: links_teams[0],
@@ -21,7 +23,28 @@ rp(options)
             players_link.push(link.attribs.href);
           }
         });
-        console.log(players_link)
+
+        //Solo Adan, de momento
+        const options2 = {
+          uri: base_url+players_link[0],
+          transform: function (body) {
+            return cheerio.load(body);
+          }
+        };
+        rp(options2)
+          .then(($) => { 
+            var player_stats = $(".matchstats tbody").find("tr")
+            var fixtures = []
+            player_stats.each(function(i, link){ 
+              fixtures.push($($(link).find("td")[1]).text())
+            });
+            
+            console.log(fixtures[0]);
+          })
+          
+
+            
+        
     })
     .catch((err) => {
         console.log(err);
